61c432ac578bca16f08f5f9438b34940c3dba59e
shanskie15 <sean.mantos@gmail.com>
2021-01-06 21:16:42 +0800

    Second Commit

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\stumos1.0\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\stumos1.0\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\Stumos1.0\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\Stumos1.0\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

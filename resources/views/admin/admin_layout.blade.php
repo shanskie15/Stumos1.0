80da87405cac603ac1b673ed053ff518249bf0eb
kaelishvali <kaelishvali@gmail.com>
2021-03-08 19:36:57 +0800

    michael last

--- resources/views/admin/admin_layout.blade.php
+++ resources/views/admin/admin_layout.blade.php
@@ -4,7 +4,6 @@
     @include('admin.nav.sidebar')
     <div id="content">
         @include('admin.nav.navbar')
-        @include('admin.import')
         @yield('content-body')
     </div>
 @endsection
